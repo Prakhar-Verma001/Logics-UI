f938c1b54976c1b8e7df10126798b4f4069c724d
Prakhar Verma <69008001+Prakhar-Verma001@users.noreply.github.com>
2024-01-05 00:14:02 +0530

    Update SideBar.js

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,4 @@
-import { List, ListItem, ListItemButton, Stack, Typography } from '@mui/material'
+import { List, ListItem, ListItemButton, Stack, Typography, Box } from '@mui/material'
 import React from 'react'
 import logo from '../Assests/New UI/logo 2logoBig.png'
 import black from '../Assests/New UI/ChartPieSlice-d.png'
@@ -8,12 +8,13 @@ import white from '../Assests/New UI/ChartPieSlice-dwhite.png'
 const SideBar = () => {
   return (
     <>
+    <Box m={1}>
         <Stack direction={'column'} spacing={2} alignItems={'center'} my={3} height={'fit-content'}>
             <img src={logo} alt='logo'/>
             <Typography variant='subtitle2' textAlign={'center'}>eSolar Plant Monitoring System</Typography>
         </Stack>
 
-        <List sx={{p:1}}>
+        <List sx={{p:0}}>
             <ListOpt text='Dashboard' />
             <ListOpt text='Analysis' />
             <ListOpt text='Monitoring' />
@@ -25,6 +26,7 @@ const SideBar = () => {
             <ListOpt text='User Details' />
             <ListOpt text='Logout' />
         </List>
+      </Box>  
     </>
   )
 }
@@ -32,14 +34,14 @@ const SideBar = () => {
 const ListOpt = (props) => {
   return (
     <>
-        <ListItem sx={{"&:hover":{borderRadius:5, bgcolor:'blue', color:'white'}, p:'4px'}}>
-            <ListItemButton>
-                <Stack direction={'row'} spacing={1} alignItems={'center'} pl={1}>
+        <ListItem sx={{"&:hover":{borderRadius:5, bgcolor:'blue', color:'white'}, p:0, my:1, mx:0}}>
+            <ListItemButton sx={{p:1}}>
+                <Stack direction={'row'} spacing={0.7} alignItems={'center'}>
                     <img src={black} alt='pics'
                     onMouseOver={e => (e.currentTarget.src = white)}
                     onMouseOut={e => (e.currentTarget.src = black)}
                     />
-                    <Typography variant='subtitle2' sx={{}}>{props.text}</Typography>
+                    <Typography variant='subtitle2' lineHeight={1.1}>{props.text}</Typography>
                 </Stack>
             </ListItemButton>
         </ListItem>
