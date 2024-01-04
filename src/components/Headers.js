034f5ac8015e8ae062a90f264453aa1c4374497b
Prakhar Verma <69008001+Prakhar-Verma001@users.noreply.github.com>
2024-01-05 00:23:45 +0530

    Update Headers.js

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -8,12 +8,13 @@ import W from '../Assests/New UI/WhatsApp Image 2023-10-21 at 11.05.png'
 const Headers = () => {
   return (
     <Box>
-        <Stack direction={'column'}>
-            <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'} p={1}>
+        <Stack direction={'column'} spacing={1}>
+            <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'} px={1} pt={1}>
                 <Stack direction={'row'} spacing={1} alignItems={'center'}>
                     <img src={SL} alt='slice' />
                     <Typography variant='h6' sx={{fontSize:'1.1rem', fontWeight:'700'}}>Dashborad</Typography>
                 </Stack>
+
                 <Stack direction={'row'} alignItems={'center'} spacing={3} display={{md:'flex', xs:'none'}}>
                     <Typography variant='subtitle2' sx={{fontSize:'1rem', fontWeight:'600'}}>Last log time : 21/10/2023</Typography>
                     <Typography variant='subtitle2' sx={{fontSize:'1rem', fontWeight:'600'}}>03:45 AM</Typography>
@@ -27,48 +28,48 @@ const Headers = () => {
                     <img src={ON} alt='Notification'/>
                     <img src={LOGO} alt='LOGO'/>
                 </Stack>
-
             </Stack>
-            <Divider sx={{mb:'5px'}}/>
 
-            <Grid container boxSizing={'border-box'} justifyContent={'space-between'} sx={{bgcolor:'#C2D9FF', px:1, py:1}}>
+            <Divider />
+
+            <Grid container boxSizing={'border-box'} justifyContent={'space-between'} sx={{bgcolor:'#C2D9FF', py:'4px'}}>
               <Grid item lg={3} md={6} xs={12} textAlign={'center'} py={{lg:0, xs:'4px'}}>
-                <Typography variant='subtitle2' color={'blue'} sx={{fontSize:'0.8rem', fontWeight:'700'}}>
+                <Typography variant='subtitle2' color={'blue'} sx={{fontSize:'0.8rem', fontWeight:'700', p:0.5}}>
                   Industrial Energy Management System
                 </Typography>
               </Grid>
               <Grid item lg={3} md={6} xs={12} textAlign={'center'} py={{lg:0, xs:'4px'}}>
-                <Typography variant='subtitle2' color={'blue'} sx={{fontSize:'0.8rem', fontWeight:'700'}}>
+                <Typography variant='subtitle2' color={'blue'} sx={{fontSize:'0.8rem', fontWeight:'700', p:0.5}}>
                   Flow Meter & Ground Water Monitoring
                 </Typography>
               </Grid>
               <Grid item lg={3} md={6} xs={12} textAlign={'center'} py={{lg:0, xs:'4px'}}>
-                <Typography variant='subtitle2' color={'blue'} sx={{fontSize:'0.8rem', fontWeight:'700'}}>
+                <Typography variant='subtitle2' color={'blue'} sx={{fontSize:'0.8rem', fontWeight:'700', p:0.5}}>
                   Smart Metering and DLMS Solution 
                 </Typography>
               </Grid>
               <Grid item lg={3} md={6} xs={12} textAlign={'center'} py={{lg:0, xs:'4px'}}>
-                <Typography variant='subtitle2' color={'blue'} sx={{fontSize:'0.8rem', fontWeight:'700'}}>
+                <Typography variant='subtitle2' color={'blue'} sx={{fontSize:'0.8rem', fontWeight:'700', p:0.5}}>
                   Industrial Energy Management System
                 </Typography>
               </Grid>
             </Grid>
 
-            <Grid container>
-              <Grid item lg={3} md={6} xs={12} p={1} pl={0}>
+            <Grid container rowGap={1}>
+              <Grid item lg={3} md={6} xs={12} p={0.5}>
                 <DetailCard
                   text1='Gulati Oil India Pvt Ltd'
                   text2='Plant Capacity : 500.31 kWp'
                 />
               </Grid>
-              <Grid item lg={3} md={6} xs={12} p={1}>
+              <Grid item lg={3} md={6} xs={12} p={0.5}>
                 <DetailCard
                   text1='Gulati Oil India Pvt Ltd'
                   text2='Plant Capacity : 500.31 kWp'
                 />
               </Grid>
-              <Grid item lg={6} md={12} width={'100%'} p={1} pr={0}>
-                <Box sx={{boxSizing:'border-box'}} width={'100%'} height={'100%'}>
+              <Grid item lg={6} md={12} width={1} pt={0.5} px={0.5}>
+                <Box sx={{boxSizing:'border-box', height:1, width:1}}>
                   <img src={W} alt='WhatsappImage' width={'100%'} height={'100%'} />
                 </Box>
               </Grid>
@@ -83,13 +84,15 @@ export default Headers
 const DetailCard = (props) => {
   return (
     <>
-      <Card sx={{p:2, height:'91%', boxSizing:'border-box', boxShadow:5, textAlign:{md:'left', xs:'center'}}}>
-          <Typography variant='subtitle1' sx={{fontSize:'1rem', fontWeight:'700'}}>
+      <Card sx={{p:2, height:1, boxSizing:'border-box', boxShadow:4, textAlign:{md:'left', xs:'center'}}}>
+        <Box>
+          <Typography variant='subtitle1' sx={{fontSize:'0.9rem', fontWeight:'700'}}>
               {props.text1}
           </Typography>
-          <Typography variant='subtitle1' sx={{fontSize:'1rem', fontWeight:'700'}}>
+          <Typography variant='subtitle1' sx={{fontSize:'0.9rem', fontWeight:'700'}}>
               {props.text2}
           </Typography>
+        </Box>  
       </Card>
     </>
   )
