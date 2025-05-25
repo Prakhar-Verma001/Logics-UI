d8d1f73354d405ec083ef85725491e5403db466a
Prakar-Verma001 <prakharverma443@gmail.com>
2025-05-25 13:47:39 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@
 // import './App.css';
 
 import DashboradPage from "./Pages/DashboradPage";
-import LoginPage from "./Pages/LoginPage";
 import LogicsForm from "./components/LogicsForm";
+import LoginPage from "./Pages/LoginPage";
+import { Footer } from "./components/Footer";
 
 // import {Typography} from '@mui/material'
 function App() {
   return (
     <>
         {/* <LoginPage /> */}
+        {/* <LogicsForm /> */}
+
         <DashboradPage />
+        <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Paper, Typography, Stack, Link, Divider } from '@mui/material';
+
+const Footer = () => {
+  return (
+    <>
+        <Paper
+          component="footer"
+          elevation={3}
+          square
+          sx={{ mt: 'auto', py: 4, px: 2, bgcolor: 'grey.100' }}
+        >
+          <Divider sx={{ mb: 2 }} />
+          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2} justifyContent="space-between" alignItems="center" marginInline={'2rem'}>
+            <Typography variant="body2" color="text.secondary">
+              © {new Date().getFullYear()} Created By <Typography component="span" color={'#1D24CA'} sx={{ fontWeight: 600 }}>Prakhar Verma</Typography>.
+            </Typography>
+
+            <Stack direction="row" spacing={6}>
+              <Link href="#" underline="hover" color="inherit">
+                Privacy Policy
+              </Link>
+              <Link href="#" underline="hover" color="inherit">
+                Terms
+              </Link>
+              <Link href="#" underline="hover" color="inherit">
+                Contact
+              </Link>
+            </Stack>
+          </Stack>
+        </Paper>
+    </>
+    
+  );
+}
+
+export { Footer }
